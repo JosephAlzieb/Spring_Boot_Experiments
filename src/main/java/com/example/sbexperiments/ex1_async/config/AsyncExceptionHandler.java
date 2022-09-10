2f975d64cfea21ba881a4d446f37427543409f15
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-09-10 11:53:24 +0200

    so we need a AsyncExceptionHandler

---
+++ src/main/java/com/example/sbexperiments/ex1_async/config/AsyncExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.sbexperiments.ex1_async.config;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+  @Override
+  public void handleUncaughtException(Throwable ex, Method method, Object... args) {
+    System.out.println("Method Name" + method.getName()
+        + "---" + Arrays.toString(args) + "----"
+        + "error Message: " + ex.getMessage());
+
+  }
+}

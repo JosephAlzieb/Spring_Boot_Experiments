d0c84825b94690fc18320cc3d213735b2c2721ea
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-09-10 11:30:11 +0200

    initial project

---
+++ src/main/java/com/example/sbexperiments/SbExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbExperimentsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbExperimentsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/sbexperiments/SbExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbExperimentsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

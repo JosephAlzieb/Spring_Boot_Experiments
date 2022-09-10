118f31972f0e7b422ecb9ba715f5b38b3c2a873c
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-09-10 11:48:24 +0200

    with @Async the controller will not wait until the execution of this method finished.

--- src/main/java/com/example/sbexperiments/ex1_async/service/NotificationService.java
+++ src/main/java/com/example/sbexperiments/ex1_async/service/NotificationService.java
@@ -4,10 +4,20 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
-
+/**
+ * Sync : Blocking Way of programming.
+ *        one thread will wait until the execution finished.
+ *
+ * Async : None-Blocking Way of programming.
+ *         one thread will not wait until the execution finished.
+ *
+ * Fire and Forget : None-Blocking Way of programming.
+ *                   this we can achieve using Async-Programming
+ */
 @Service
 public class NotificationService {
 
+  @Async
   public void sendTextMessage(String mobile) {
 
     // dummy logic to call 3rd party services

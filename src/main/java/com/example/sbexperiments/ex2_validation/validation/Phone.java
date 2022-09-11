0d950eaab2bad72800dfd0f981ff20600281e091
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-09-11 16:54:52 +0200

    the annotation is finish, but not yet implemented

--- src/main/java/com/example/sbexperiments/ex2_validation/validation/Phone.java
+++ src/main/java/com/example/sbexperiments/ex2_validation/validation/Phone.java
@@ -1,5 +1,20 @@
 package com.example.sbexperiments.ex2_validation.validation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PhoneValidator.class)
+@Documented
 public @interface Phone {
+  String message() default "Please check your phone number ${validatedValue}";
+  Class<?>[] groups() default {};
+  Class<? extends Payload>[] payload() default {};
 
 }

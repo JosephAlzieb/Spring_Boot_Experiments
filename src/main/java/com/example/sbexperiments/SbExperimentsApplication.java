87c4e5c7276b8614b8f0db97c88ac33522754a7f
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-09-12 22:46:08 +0200

    enable retryable

--- src/main/java/com/example/sbexperiments/SbExperimentsApplication.java
+++ src/main/java/com/example/sbexperiments/SbExperimentsApplication.java
@@ -2,8 +2,10 @@ package com.example.sbexperiments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class SbExperimentsApplication {
 
   public static void main(String[] args) {
